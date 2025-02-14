6689b193a026b0aba67f10df546778a04cf6b074
Henry Strickland <strick@yak.net>
2025-02-14 01:07:33 -0500

    forgot spin.h

---
+++ kernel/spin.h
@@ -0,0 +1,11 @@
+// Spinors are just for visualization and debugging.
+// They create a "spinning" character on the Console top bar.
+// They should be of no consequence.
+
+inline void Spin(byte b) { Cons[b]++; }
+
+inline void SpinCWait() { Spin(2); }
+
+inline void SpinIrq() { Spin(20); }
+inline void SpinRealSeconds() { Spin(28); }
+inline void SpinBreakkey() { Spin(30); }
